5fbeb1d5331a16c8669b611fc3c13b0b4c87dee0
iZeaoGamer <voidminerpocket@gmail.com>
2020-12-31 20:49:26 +0000

    Fix syntax error. >:(

--- src/xenialdan/UserManager/listener/BanListener.php
+++ src/xenialdan/UserManager/listener/BanListener.php
@@ -38,9 +38,9 @@ class BanListener implements Listener
             $date2 = Network::timeFormat($ban->getUntil());
             $msg = TextFormat::DARK_RED . TextFormat::BOLD . "You are banned!";
             $msg .= TextFormat::RED . "\nReason: " . TextFormat::GRAY . $ban->reason;
-            $msg .= TextFormat::RED . "\nBanned since: " . TextFormat::GRAY . $date);
+            $msg .= TextFormat::RED . "\nBanned since: " . TextFormat::GRAY . $date;
        
-            $msg .= TextFormat::RED . "\nBanned Until: " . TextFormat::GRAY .  $date2);
+            $msg .= TextFormat::RED . "\nBanned Until: " . TextFormat::GRAY .  $date2;
             $debug = "Banned user tried to log in:" . TextFormat::EOL . $ban;
             $kick = false;
             if ($ban->isTypeBanned(Ban::TYPE_IP) && $user->getIP() === $player->getAddress()) {
